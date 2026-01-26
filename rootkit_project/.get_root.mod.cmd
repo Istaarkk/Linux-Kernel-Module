074f806753590d6837003b23dad719c2f91ec604
Istaarkk <Matthieu.Dacruz@protonmail.com>
2026-01-26 17:27:00 +0100

    Initial push

--- rootkit_project/.get_root.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_get_root.mod := printf '%s\n'   get_root.o | awk '!x[$$0]++ { print("./"$$0) }' > get_root.mod
--- rootkit_project/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo get_root.o; :; } > modules.order
